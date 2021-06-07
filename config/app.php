93e709c510d76ac26fb94a0231780aabcbb321c4
hossam hassan <hossamhassan889@gmail.com>
2021-06-07 02:30:22 +0200

    v

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://ercelearning.com/erc/erc/'),
 
     'asset_url' => env('ASSET_URL', null),
 
